723c048fdd0c83ae95787b28b6864736f055c83b
MLozyy <MLozovyywd@gmail.com>
2020-02-29 10:59:56 -0500

    Initial Commit

---
+++ stream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stream
+//
+//  Created by Mikhail Lozovyy on 2/29/20.
+//  Copyright © 2020 Mikhail Lozovyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
